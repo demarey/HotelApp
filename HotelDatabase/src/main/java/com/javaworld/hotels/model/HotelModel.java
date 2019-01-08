07c29a1807b919b87ea52bb359c4f94b8da04a31
YannGarbe <yanngarbe@gmail.com>
2019-01-08 09:57:25 +0100

    Adding a new hotel

--- HotelDatabase/src/main/java/com/javaworld/hotels/model/HotelModel.java
+++ HotelDatabase/src/main/java/com/javaworld/hotels/model/HotelModel.java
@@ -42,6 +42,7 @@ public class HotelModel {
 		new Hotel("Hotel Vendome","Place Vendome","Paris",5),
 		new Hotel("Hotel Hilton","Trafalgar Square","London",4),
 		new Hotel("Hotel Ibis","The City","London",3),
+		new Hotel("Hotel Cigogne","Grand place","",2),
 	};
 	
 	/**
